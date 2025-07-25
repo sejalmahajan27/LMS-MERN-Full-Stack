1ebb3315420815e38edcbf8fe9bcca3dea99aff3
Sejal Mahajan <sejalmahajan2005@example.com>
2025-07-25 18:07:49 +0530

    Added new features or updates

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -18,30 +18,29 @@ export const clerkWebhooks = async(req,res)=> {
                     _id: data.id,
                     email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
-                    imageUrl: data.imageUrl,
+                    imageUrl: data.image_url,
                 }
                 await User.create(userData)
                 res.json({})
                 break;
             }
-                
-               case 'user.updated':{
+
+            case 'user.updated': {
                 const userData = {
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
-                    imageUrl: data.imageUrl,
+                    imageUrl: data.image_url,
                 }
-                await  User.findByIdAndUpdate(data.id, userData)
+                await User.findByIdAndUpdate(data.id, userData)
                 res.json({})
                 break;
-             }
-        
+            }
 
-             case 'user.deleted' : {
+            case 'user.deleted': {
                 await User.findByIdAndDelete(data.id)
                 res.json({})
                 break;
-             }
+            }
 
             default:
                 break;
